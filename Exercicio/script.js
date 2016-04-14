4881f7c537f11322049661dc0c6e1bb4cabfe130
izabeladnz <izabela_pagani@hotmail.com>
2016-04-14 10:53:12 -0300

    Update script.js

--- Exercicio/script.js
+++ Exercicio/script.js
@@ -7,4 +7,53 @@ $(document).ready(function(e){
 	});
 });
 
-
+function dialogo(){
+	$("#dialog-confirm").dialog({
+		resizable:false,
+		width:400,
+		height:200,
+		modal:true,
+		buttons:{
+			"Confirma": function(){
+				var nome = document.querySelector("#nome").value;
+				var tamnome = nome.length;
+				var validanome = false;
+				for(var i=0; i<=tamnome; i++){
+					if ( nome[i] == ' '){
+						var n = nome.split(' ');
+						if ( n[0] >= 3){
+							if ( n[1] >= 4){
+								validanome = true;
+							}
+						}
+					}
+				}
+				var email = document.querySelector("#email").value;
+				var tamemail = email.length;
+				var validaemail = false;
+				for(var j=0; j<=tamemail; j++){
+					if ( email[j] == '@'){
+						var e = email.split('@');
+						if ( e[0] >= 3){
+							if ( e[1] >= 4){
+								validaemail = true;
+							}
+						}
+					}
+				}
+				if(validanome==true && validaemail==true){
+					alert('Dados gravados!')
+				}else {
+					alert ('Dados errados!')
+				}
+				
+				
+				$(this).dialog("close");
+			},
+			Cancelar: function(){
+				$(this).dialog("close");				
+			}
+		}
+	});
+	
+};
