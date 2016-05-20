df2bc899408e733d00a375a6502ec65c5e17e6f3
izabeladnz <izabela_pagani@hotmail.com>
2016-05-20 11:21:34 -0300

    Update cadCliente.php

--- Formularios/cadCliente.php
+++ Formularios/cadCliente.php
@@ -69,7 +69,28 @@
 				$camposOK = false;
 				}
             
-							 //COMPLETAR
+    	$cpf = str_pad(ereg_replace('[^0-9]', '', $cpf), 11, '0', STR_PAD_LEFT);
+	
+    	if (strlen($cpf) != 11 || $cpf == '00000000000' || $cpf == '11111111111' || 
+		$cpf == '22222222222' || $cpf == '33333333333' || $cpf == '44444444444' || 
+		$cpf == '55555555555' || $cpf == '66666666666' || $cpf == '77777777777' || 
+		$cpf == '88888888888' || $cpf == '99999999999') {
+			$camposOK = false;
+			echo "CPF Errado!<br>";		
+   		} else {   
+     		for ($t=9;$t<11;$t++) {
+         	 	for ($d=0,$c=0;$c<$t;$c++) {
+					$d += $cpf{$c} * (($t+1)-$c);
+          		}
+
+         		$d = ((10 * $d) % 11) % 10;
+ 
+           		if ($cpf{$c} != $d) {
+                	$camposOK = false;
+					echo "CPF Errado!<br>";					
+          		  }
+        	} 
+   		}
           
              if($camposOK){
              	echo "<table border='0' cellpadding='5'>";
